e954ed7acc498ed6acd28ceb7127bae4644a2fd2
Justice <jdlammer@ucsc.edu>
2017-02-08 18:00:38 -0800

    Added image ability for projects
    Forgot to add upload an image so did

--- db.py
+++ db.py
@@ -18,6 +18,7 @@ db.define_table('project',
                 Field('person_id', 'reference person'),
                 Field('title'),
                 Field('body', 'text'),
+                Field('image', 'upload'),
                 Field('created_on', 'datetime', default=request.now),
                 Field('created_by', 'reference auth_user', default=auth.user_id),
                 format='%(title)s')
